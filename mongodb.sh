bf649390a16a9c88c1cd7bcdffb95a3837a8d5f2
Jayani22 <mutcherlajayani@gmail.com>
2025-10-08 07:52:17 +0530

    session-06

--- mongodb.sh
+++ mongodb.sh
@@ -30,7 +30,7 @@ VALIDATE(){ # functions receive inputs through args
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding Mongo repo"
 
-dnf install mongodb-org -y &>>LOG_FILE
+dnf install mongodb-org -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB"
 
 systemctl enable mongodb &>>$LOG_FILE
