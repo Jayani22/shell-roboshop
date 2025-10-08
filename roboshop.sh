5495d4bca3f5d4641b04f579a15ea3a36969b3b2
Jayani22 <mutcherlajayani@gmail.com>
2025-10-08 06:39:02 +0530

    session-06

--- roboshop.sh
+++ roboshop.sh
@@ -4,6 +4,7 @@ AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-059eb64c5a56f1a51" 
 ZONE_ID="Z08769191SZ0EERXMPHL9"
 DOMAIN_NAME="jayani23.fun"
+
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
@@ -14,7 +15,7 @@ do
         RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.jayani23.fun
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME" # jayani23.fun
+        RECORD_NAME="$DOMAIN_NAME" # jayani23.fun
     fi
 
     echo "$instance: $IP"
@@ -26,11 +27,11 @@ do
             "Comment": "Updating record set"
             ,"Changes": [{
             "Action"                 : "UPSERT"
-		    , "ResourceRecordSet"    : {
+		    ,"ResourceRecordSet"    : {
 		            "Name"			 : "'$RECORD_NAME'"
-		            "Type"			 : "A"
-		            "TTL"			 : 1
-		            "ResourceRecords": [{
+		            ,"Type"			 : "A"
+		            ,"TTL"			 : 1
+		            ,"ResourceRecords": [{
 				        "Value"    	 : "'$IP'"
                     }]
             }
