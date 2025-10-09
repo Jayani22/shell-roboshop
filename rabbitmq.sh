68a8fe5265516c1cc4586c170f3ce5f452d8d2fb
Jayani22 <mutcherlajayani@gmail.com>
2025-10-09 12:37:50 +0530

    some matter

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -47,5 +47,5 @@ rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 VALIDATE $? "Setting up permissions"
 
 END_TIME=$(date +%s)
-TOTAL_TIME = $(( $END_TIME - $START_TIME))
+TOTAL_TIME=$(( $END_TIME - $START_TIME))
 echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
