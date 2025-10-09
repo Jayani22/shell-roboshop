6718d4254b075ef9d06d670c419443737553d1db
Jayani22 <mutcherlajayani@gmail.com>
2025-10-09 08:51:16 +0530

    something updated

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -56,13 +56,12 @@ systemctl daemon-reload
 systemctl enable catalogue &>>$LOG_FILE
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongosh -y &>>$LOG_FILE
+dnf install mongodb-mongoshsfds -y &>>$LOG_FILE
 
 
 INDEX=$(mongosh mongodb.jayani23.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
-    VALIDATE $? "Load catalogue products"
 else
     echo -e "Catalogue products already loaded... $Y SKIPPING $N"
 fi
